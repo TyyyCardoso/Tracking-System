5d932a0a74389681b2a5a1857554cf29b1554d40
TyyyCardoso <86069519+TyyyCardoso@users.noreply.github.com>
2021-07-08 11:56:05 +0000

    Update mainactivity to add map functionalities

--- App/app/src/main/java/tyyy/cardoso/evt_tracking_system/MainActivity.kt
+++ App/app/src/main/java/tyyy/cardoso/evt_tracking_system/MainActivity.kt
@@ -1,11 +1,93 @@
 package tyyy.cardoso.evt_tracking_system
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private var lat : Double = 0.0
+    private var long : Double = 0.0
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         *
+         * PUT THE STATUS BAR FOREGROUND COLOR TO BLACK
+         *
+         * DEPRECATED
+         *
+         * */
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        };//  set status text dark
+
+
+        /**
+         *
+         * INITALIZATING THE GOOGLE MAP
+         *
+         */
+
+        //val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+        //mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+
+        if(true/*Constants.isLocationAvailable(this)*/){
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location ->
+                    if (location != null) {
+                        lat = location.latitude
+                        long = location.longitude
+                        val cardoso = LatLng(lat, long)
+                        var zoomLevel = 16.0f
+                        //mMap.addMarker(MarkerOptions().position(cardoso).title("Marker in CARDOSO").icon(bitmapDescriptorFromVector(this, R.drawable.ic_baseline_directions_bus_24)))
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cardoso,zoomLevel))
+                    }
+                }
+        }else{
+            //Constants.isLocationAvailable(this)
+        }
+
+    }
+
+    private fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+        return ContextCompat.getDrawable(context, vectorResId)?.run {
+            setBounds(0, 0, intrinsicWidth, intrinsicHeight)
+            val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+            draw(Canvas(bitmap))
+            BitmapDescriptorFactory.fromBitmap(bitmap)
+        }
+    }
+
 }
