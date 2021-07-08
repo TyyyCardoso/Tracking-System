933c9a9d860c7c828330300935b31c6d15978918
TyyyCardoso <86069519+TyyyCardoso@users.noreply.github.com>
2021-07-08 11:32:56 +0000

    Add handler in splashscreen activity
    Added a handler to wait 3 seconds to change between activities
    
    //Handler is deprecated - Update as soon as possible

--- App/app/src/main/java/tyyy/cardoso/evt_tracking_system/SplashScreen.kt
+++ App/app/src/main/java/tyyy/cardoso/evt_tracking_system/SplashScreen.kt
@@ -1,11 +1,23 @@
 package tyyy.cardoso.evt_tracking_system
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        /**
+         * Uses Handler for making a delay before switching between activities
+         */
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
     }
 }
